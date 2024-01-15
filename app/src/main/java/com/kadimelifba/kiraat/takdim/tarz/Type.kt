5dc33fc71a0ba680e8e4d976d5da40612072713f
Tayfun Yasar <ysrtayfun@gmail.com>
2024-01-15 15:39:21 +0300

    amiri hat ilave edildi.

--- app/src/main/java/com/kadimelifba/kiraat/takdim/tarz/Type.kt
+++ app/src/main/java/com/kadimelifba/kiraat/takdim/tarz/Type.kt
@@ -2,9 +2,19 @@ package com.kadimelifba.kiraat.takdim.tarz
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kadimelifba.kiraat.R
+
+val AmiriHatAilesi = FontFamily(
+    Font(R.font.amiri_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.amiri_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.amiri_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.amiri_bold_italic, FontWeight.Bold, FontStyle.Italic)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +42,44 @@ val Typography = Typography(
     )
     */
 )
+
+
+val Typography.kadimElifbaTemel: TextStyle
+    get() = TextStyle(
+        fontFamily = AmiriHatAilesi,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+
+val Typography.kadimElifbaKalin: TextStyle
+    get() = TextStyle(
+        fontFamily = AmiriHatAilesi,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+
+val Typography.kadimElifbaEgik: TextStyle
+    get() = TextStyle(
+        fontFamily = AmiriHatAilesi,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    )
+
+val Typography.kadimElifbaKalinEgik: TextStyle
+    get() = TextStyle(
+        fontFamily = AmiriHatAilesi,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        fontSize = 28.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.5.sp
+    )
